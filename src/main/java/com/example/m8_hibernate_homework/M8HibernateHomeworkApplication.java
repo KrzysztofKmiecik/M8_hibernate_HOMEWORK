7f74b3778c4d7dd4d349f421b621792bd7825a41
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-06-14 12:00:44 +0200

    Initial commit

---
+++ src/main/java/com/example/m8_hibernate_homework/M8HibernateHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.m8_hibernate_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M8HibernateHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M8HibernateHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/m8_hibernate_homework/M8HibernateHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m8_hibernate_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M8HibernateHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
